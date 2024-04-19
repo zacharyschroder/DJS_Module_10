5f1d46cc5057b5e807b51ee2e8fde9a21978f390
zacharyschroder <zachary@codespace.co.za>
2024-04-19 08:07:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// URL:  http://dog-api.kinduff.com/api/facts
+
+// TASK 1
+// Step 1: Use the fetch API and .then syntax to retrieve a fact from the Dog Facts API
+// Step 2: Catch any errors
+
+// Your code goes here
+
+// TASK 2
+// Step 1: Use the fetch API and async/await syntax to retrieve a fact from the Dog Facts API
+// Step 2: Catch any errors
+
+// Your code goes here
